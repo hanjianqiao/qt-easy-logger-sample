9ba2b550fa309d3cef4c1279d13441ceba328a95
Jianqiao Han <hanjianqiao@gmail.com>
2024-04-01 21:13:29 +0800

    update

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
 
     qInstallMessageHandler(h::Logger::messageHandler);
+    h::Logger::setFilePathSkipCharCount(QString("D:/repos/qt-easy-logger-sample/").size());
     qDebug() << "这是测试输出";
 
     SampleWindow w;
